d674c12c3758250d4185c7de28deb4873bcbbdaa
Roman Skydan <roma.skidan@gmail.com>
2019-06-06 15:02:20 -0500

    initial commit

---
+++ config/config.development.ts
@@ -0,0 +1,12 @@
+export const config = {
+    database: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'nest',
+        logging: false,
+    },
+    jwtPrivateKey: 'jwtPrivateKey',
+};
